bc9e29e33d35ef03ba487d4f5f27c3444d302885
pascalbetz <pascal.betz@simplificator.com>
2010-09-07 00:03:41 +0200

    added doc

--- lib/truesenses/config.rb
+++ lib/truesenses/config.rb
@@ -1,14 +1,25 @@
 module Truesenses
   class Config
-    attr_reader :password, :username, :protocol
+    attr_reader :password, :username, :protocol, :origin, :deliver
+    
+    # :username
+    # :password
+    # :protocol :http or :https, defaults to :http
+    # :origin default origin ID of text message
+    # :deliver true|false, false turns on the TEST mode of truesenses API
     def initialize(options)
       options = options.stringify_keys
       @password = options['password']
       @username = options['username']
       @protocol = options['protocol'] || :http
+      @origin = options['origin']
+      @deliver = options.has_key?('deliver') ? options['deliver'] : true
       verify!
     end
-
+    
+    # Load config from a YAML file
+    # if root is specified, then this root key is used (useful when you store different configurations
+    # in one YAML file: Config.load('somefile.yml', RAILS_ENV))
     def self.load(filename, root = nil)
       data = YAML.load_file(filename)
       Config.new(root ? data[root] : data)
